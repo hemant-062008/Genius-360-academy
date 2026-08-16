3e87e6b12bfbd1c3605689fe5267c948dd11864c
Hemant Kushwaha <hemant.shri2008@gmail.com>
2026-08-16 16:49:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+/* ================= MOBILE MENU ================= */
+
+const menuBtn = document.getElementById("menuBtn");
+const navbar = document.getElementById("navbar");
+
+menuBtn.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+    const icon = menuBtn.querySelector("i");
+
+    if (navbar.classList.contains("active")) {
+
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+
+    } else {
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    }
+
+});
+
+
+/* Close mobile menu after clicking */
+
+document.querySelectorAll("#navbar a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navbar.classList.remove("active");
+
+        const icon = menuBtn.querySelector("i");
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    });
+
+});
+
+
+/* ================= CURRENT YEAR ================= */
+
+document.getElementById("year").textContent =
+    new Date().getFullYear();
